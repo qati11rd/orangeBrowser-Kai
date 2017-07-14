27c5fee61797a1306ecd6356828977dbac06e8f8
qati11rd <qwerty.at.iron+github@gmail.com>
2017-07-15 03:24:41 +0900

    設定値追加等

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,23 +8,151 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace orangeBrowser_Kai.Properties
-{
-
-
-	[global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "10.0.0.0")]
-	internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-	{
-
-		private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-		public static Settings Default
-		{
-			get
-			{
-				return defaultInstance;
-			}
-		}
-	}
+namespace orangeBrowser_Kai.Properties {
+    
+    
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "10.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+        
+        public static Settings Default {
+            get {
+                return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
+        public global::System.Drawing.Point Window_Point {
+            get {
+                return ((global::System.Drawing.Point)(this["Window_Point"]));
+            }
+            set {
+                this["Window_Point"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("600, 400")]
+        public global::System.Drawing.Size Window_Size {
+            get {
+                return ((global::System.Drawing.Size)(this["Window_Size"]));
+            }
+            set {
+                this["Window_Size"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("100")]
+        public double Window_Opacity {
+            get {
+                return ((double)(this["Window_Opacity"]));
+            }
+            set {
+                this["Window_Opacity"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://www.google.co.jp/")]
+        public string General_HomePage {
+            get {
+                return ((string)(this["General_HomePage"]));
+            }
+            set {
+                this["General_HomePage"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://www.nicovideo.jp/search/%s")]
+        public string General_SearchPage {
+            get {
+                return ((string)(this["General_SearchPage"]));
+            }
+            set {
+                this["General_SearchPage"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool General_HideUrl {
+            get {
+                return ((bool)(this["General_HideUrl"]));
+            }
+            set {
+                this["General_HideUrl"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool General_VerifyClose {
+            get {
+                return ((bool)(this["General_VerifyClose"]));
+            }
+            set {
+                this["General_VerifyClose"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string Nico_Mail {
+            get {
+                return ((string)(this["Nico_Mail"]));
+            }
+            set {
+                this["Nico_Mail"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string Nico_Password {
+            get {
+                return ((string)(this["Nico_Password"]));
+            }
+            set {
+                this["Nico_Password"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool Nico_AllowLowMode {
+            get {
+                return ((bool)(this["Nico_AllowLowMode"]));
+            }
+            set {
+                this["Nico_AllowLowMode"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string Nico_DefaultPath {
+            get {
+                return ((string)(this["Nico_DefaultPath"]));
+            }
+            set {
+                this["Nico_DefaultPath"] = value;
+            }
+        }
+    }
 }
