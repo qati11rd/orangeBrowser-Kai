1056f817040b0f139b2e849ee30e3210d86c8759
qati11rd <qwerty.at.iron+github@gmail.com>
2017-12-28 03:04:33 +0900

    String半角全角変換拡張

---
+++ orangeBrowser-Kai/util/Extensions/String.cs
@@ -0,0 +1,12 @@
+﻿
+using CSharp.Japanese.Kanaxs;
+
+namespace orangeBrowser_Kai.util
+{
+	static partial class Extensions
+	{
+		public static string ToHankaku(this string str) => KanaEx.ToHankaku(str);
+
+		public static string ToZenkaku(this string str) => KanaEx.ToZenkaku(str);
+	}
+}
